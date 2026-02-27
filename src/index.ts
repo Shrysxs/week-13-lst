00f9926a91709812a475d170d2bba5dc14c1547a
Shrysxs <shreyashborade2@gmail.com>
2026-02-27 22:51:20 +0530

    parsed

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,31 @@ import { burnTokens, mintTokens, sendNativeTokens } from './mintTokens';
 
 const app = express();
 
+const HELIUS_RESPONSE = {
+    nativeTransfers": [ { 
+    "amount": 72936000000,
+    "fromUserAccount": "CKs1E69a2e9TmH4mKKLrXFF8kD3ZnwKjoEuXa6sz9WqX", 
+    "toUserAccount": "AAaTGaA3uVqikfVEwoSG7EwkCb4bBDsMEyueiVUS5CaU"
+}
 
 app.post('/helius', async(req, res) => {
-    const fromAddress = req.body.fromAddress;
-    const toAddress = req.body.toAddress;
-    const amount = req.body.amount;
-    const type = "received_native_sol";
-
-    if (type === "received_native_sol") {
-        await mintTokens(fromAddress, toAddress, amount);
-    } else {
-        // What could go wrong here?
-        await burnTokens(fromAddress, toAddress, amount);
-        await sendNativeTokens(fromAddress, toAddress, amount);
+    const incomingTx = HELIUS_RESPONSE.nativeTransfers.find(x => x.toUserAccount === VALUT)
+    if (!incomingTx){
+       res.json({message: "processed"})
+       return
     }
+    const fromAddress = incomingTx.fromUserAccount;
+    const toAddress = VALUT;
+    const amount = incomingTx.amount;
+    const type = "received_native_sol";
+      await mintTokens(fromAddress, toAddress, amount);
+    //if (type === "received_native_sol") {
+      
+   // } else {
+        // What could go wrong here?
+      //  await burnTokens(fromAddress, toAddress, amount);
+        //await sendNativeTokens(fromAddress, toAddress, amount);
+    //}
 
     res.send('Transaction successful');
 });
